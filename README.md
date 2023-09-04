4ca47ab5ab8d1caddbde8aac58466d0fec289702
Sammy Chalegre <57070354+sammysc@users.noreply.github.com>
2023-09-04 01:34:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Projeto curso,dev 
+
+Projeto curso,dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Se vc me ama vem aqui me dá um beijo...</h1>
+  return <h1>Se vc me ama vem aqui me dá um beijo...</h1>;
+}
+
+function teste2323() {
+  console.log("indetren");
 }
 
 export default Home;
